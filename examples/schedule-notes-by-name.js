997cf672fe65c8527d44329307f0a846f4ff7948
Adam Murray <adamjmurray@gmail.com>
2016-06-16 22:22:56 -0700

    minor tweaks

--- examples/schedule-notes-by-name.js
+++ examples/schedule-notes-by-name.js
@@ -1,13 +1,15 @@
 require('../src/globals');
-const Scheduler = require('../src/time/scheduler');
+const { Scheduler } = require('../src/time');
 const { MIDIOut } = require('../src/midi');
-const out = new MIDIOut({ defaultDuration: 200 });
-out.open();
-const note = out.note.bind(out);
 
-const s = new Scheduler();
-s.at(   0, () => note(C4) || note(E4));
-s.at( 500, () => note(E4) || note(G4));
-s.at(1000, () => note(G4) || note(C5));
-s.at(1500, () => note(C5) || note(Eb4) || note(C3));
-s.start();
+const scheduler = new Scheduler();
+const output = new MIDIOut({ defaultDuration: 200 });
+const note = output.note.bind(output);
+
+scheduler.at(   0,ms => note(C4) & note(E4));
+scheduler.at( 500,ms => note(E4) & note(G4));
+scheduler.at(1000,ms => note(G4) & note(C5));
+scheduler.at(1500,ms => note(C5) & note(Eb4) & note(C3));
+
+output.open();
+scheduler.start();
--- examples/schedule-notes.js
+++ examples/schedule-notes.js
@@ -1,11 +1,14 @@
-const Scheduler = require('../src/time/scheduler');
+const { Scheduler } = require('../src/time');
 const { MIDIOut } = require('../src/midi');
-const out = new MIDIOut({ duration: 250 });
-out.open();
 
-const s = new Scheduler();
-s.at(   0, () => out.note(60) || out.note(64));
-s.at( 500, () => out.note(64) || out.note(67));
-s.at(1000, () => out.note(67) || out.note(72));
-s.at(1500, () => out.note(72) || out.note(60) || out.note(48));
-s.start();
+const output = new MIDIOut({ defaultDuration: 400 });
+const note = output.note.bind(output);
+const scheduler = new Scheduler();
+
+scheduler.at(   0,ms => note(60) & note(64));
+scheduler.at( 500,ms => note(64) & note(67));
+scheduler.at(1000,ms => note(67) & note(72));
+scheduler.at(1500,ms => note(72) & note(60) & note(48));
+
+output.open();
+scheduler.start();
