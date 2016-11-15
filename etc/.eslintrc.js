5a0ded1066995b8c73f5403be4203569db950407
Adam Murray <adamjmurray@gmail.com>
2016-11-14 20:16:31 -0800

    Fail the build if a mocha test calls .only() to skip the rest of the test suite

--- etc/.eslintrc.js
+++ etc/.eslintrc.js
@@ -5,7 +5,11 @@ module.exports = {
     mocha: true,
   },
   extends: "eslint:recommended",
+  plugins: [
+    "mocha-no-only"
+  ],
   rules: {
     "no-console": 1,
+    "mocha-no-only/mocha-no-only": ["error"],
   }
 };
